b06b5aa349998111f6c73c2d24ce81375e8c7222
Alain Tresor <tresoralain35@gmail.com>
2022-09-13 12:47:07 +0200

    fixing heroku

--- app/Http/Controllers/orderController.php
+++ app/Http/Controllers/orderController.php
@@ -9,11 +9,7 @@ use Illuminate\Http\Request;
 
 class orderController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
         //
